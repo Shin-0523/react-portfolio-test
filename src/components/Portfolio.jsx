ff1dfb72cd994c068c00879bac54dcd386c0b922
Shin-0523 <kobo451@gmail.com>
2026-03-04 13:27:15 +0900

    react-portfolio-test

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { PROJECTS } from "../constants";
+import { PROJECTS, BASE_URL } from "../constants"; // 追加
  
 const Portfolio = () => {
   const [selectedProject, setSelectedProject] = useState(PROJECTS[1]);
@@ -43,7 +43,7 @@ const Portfolio = () => {
           {/* プロジェクト詳細 */}
           <div className="w-full col-span-2">
             <img
-              src={selectedProject.img}
+              src={BASE_URL + selectedProject.img} // 追加
               alt={selectedProject.title}
               className="rounded-t-xl border border-stone-400 border-b-0 h-[380px] w-full object-cover"
             />
